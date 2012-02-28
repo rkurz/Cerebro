70213ceaea67830d66bab15ec69d2d4c771850dd
Rob Kurz <kurz0470@yahoo.ca>
2012-02-27 22:00:54 -0500

    - increased width of test case component to fit column - TargetProcessFactory now loads data based on the current date (instead of fixed date used during testing)

--- Cerebro/DataFactories/TargetProcessFactory.cs
+++ Cerebro/DataFactories/TargetProcessFactory.cs
@@ -13,7 +13,9 @@ namespace Cerebro.DataFactories
         public static Iteration GetCurrentIteration()
         {
             var serializer = new JavaScriptSerializer();
-            var response = HttpUtilities.HttpGet("http://creativeop.tpondemand.com/api/v1/Projects/2648/Iterations/?where=(StartDate lte '2012-2-25') and (EndDate gte '2012-2-25')&include=[Name, StartDate, EndDate, UserStories[Name, EntityState]]");
+            var url = string.Format("http://creativeop.tpondemand.com/api/v1/Projects/2648/Iterations/?where=(StartDate lte '{0:s}') and (EndDate gte '{0:s}')&include=[Name, StartDate, EndDate, UserStories[Name, EntityState]]", DateTime.Now.ToString("MM/dd/yyyy"));
+            //var response = HttpUtilities.HttpGet("http://creativeop.tpondemand.com/api/v1/Projects/2648/Iterations/?where=(StartDate lte '2012-2-25') and (EndDate gte '2012-2-25')&include=[Name, StartDate, EndDate, UserStories[Name, EntityState]]");
+            var response = HttpUtilities.HttpGet(url);
             try
             {
                 var iterations = serializer.Deserialize<ListResponse<Iteration>>(response);
@@ -28,7 +30,9 @@ namespace Cerebro.DataFactories
         public static List<Assignable> GetTasksForCurrentIteration()
         {
             var serializer = new JavaScriptSerializer();
-            var response = HttpUtilities.HttpGet("http://creativeop.tpondemand.com/api/v1/Assignables?include=[Times[Remain,Spent,Date],Id,Name,Description,Effort,EffortCompleted,EffortToDo,EntityType,Iteration[Name,StartDate,EndDate]]&where=(Project.Id eq 2648) and (Iteration.StartDate lte '2012-2-25') and (Iteration.EndDate gte '2012-2-25')");
+            var url = string.Format("http://creativeop.tpondemand.com/api/v1/Assignables?include=[Times[Remain,Spent,Date],Id,Name,Description,Effort,EffortCompleted,EffortToDo,EntityType,Iteration[Name,StartDate,EndDate]]&where=(Project.Id eq 2648) and (Iteration.StartDate lte '{0:s}') and (Iteration.EndDate gte '{0:s}')", DateTime.Now.ToString("MM/dd/yyyy"));
+            //var response = HttpUtilities.HttpGet("http://creativeop.tpondemand.com/api/v1/Assignables?include=[Times[Remain,Spent,Date],Id,Name,Description,Effort,EffortCompleted,EffortToDo,EntityType,Iteration[Name,StartDate,EndDate]]&where=(Project.Id eq 2648) and (Iteration.StartDate lte '2012-2-25') and (Iteration.EndDate gte '2012-2-25')");
+            var response = HttpUtilities.HttpGet(url);
             try
             {
                 var tasks = serializer.Deserialize<ListResponse<Assignable>>(response);
